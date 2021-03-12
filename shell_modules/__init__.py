e2726bb90b36452256a3213d99dfd01c18ae6096
Tre' Zareck <trezar001@gmail.com>
2021-03-11 23:38:55 -0500

    tty module

--- shell_modules/__init__.py
+++ shell_modules/__init__.py
@@ -1,8 +1,8 @@
 #put all your modules here or they won't work nicely!
 
 import shell_modules.sample as sample
-import shell_modules.upgrade as upgrade
+import shell_modules.pshell_download as pshell_download
 import shell_modules.tty_shell as tty
 
 def get_modules():
-     return [sample, upgrade, tty]
+     return [sample, pshell_download, tty]
